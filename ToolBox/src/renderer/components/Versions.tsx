2230face2d8ea3877aa5e99951fd536e3ade8d2e
七星玉蘅 <1532250426@qq.com>
2025-02-07 22:19:15 +0800

    append: :heavy_plus_sign: 安装 Ant Design
    安装 React 版本组件库

--- ToolBox/src/renderer/components/Versions.tsx
+++ ToolBox/src/renderer/components/Versions.tsx
@@ -1,3 +1,4 @@
+import { Button, Flex } from 'antd'
 import { useState } from 'react'
 
 function Versions(): JSX.Element {
@@ -5,6 +6,26 @@ function Versions(): JSX.Element {
 
   return (
     <ul className="versions">
+      <Flex gap="small" wrap>
+        <Button color="primary" variant="solid">
+          Solid
+        </Button>
+        <Button color="primary" variant="outlined">
+          Outlined
+        </Button>
+        <Button color="primary" variant="dashed">
+          Dashed
+        </Button>
+        <Button color="primary" variant="filled">
+          Filled
+        </Button>
+        <Button color="primary" variant="text">
+          Text
+        </Button>
+        <Button color="primary" variant="link">
+          Link
+        </Button>
+      </Flex>
       <li className="electron-version">Electron v{versions.electron}</li>
       <li className="chrome-version">Chromium v{versions.chrome}</li>
       <li className="node-version">Node v{versions.node}</li>
